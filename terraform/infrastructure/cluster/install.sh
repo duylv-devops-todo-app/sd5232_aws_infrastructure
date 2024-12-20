23e37c53c4b701920d29f93717d49ecdbf13f2c8
Duy Le Van <duy.levan@nashtechglobal.com>
2024-12-20 13:34:49 +0700

    add terraform to setup infra

--- terraform/infrastructure/cluster/install.sh
+++ terraform/infrastructure/cluster/install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-sudo yum update
+#sudo yum update
 
 # Install Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo dnf install java-17-amazon-corretto -y
-sudo yum install jenkins -y
+#sudo wget -O /etc/yum.repos.d/jenkins.repo \
+#    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+#sudo yum upgrade
+#sudo dnf install java-17-amazon-corretto -y
+#sudo yum install jenkins -y
 
 # Start Jenkins
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+#sudo systemctl enable jenkins
+#sudo systemctl start jenkins
 
 # Install git
-sudo yum install git -y
+#sudo yum install git -y
 
-# Install docker
+# Install git
 #sudo yum install docker
 #sudo systemctl start docker
 #sudo usermod -a -G docker jenkins
